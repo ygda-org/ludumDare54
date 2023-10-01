80aaf18b1f62780afa72df7c7e4247581be83bad
ToastedFood <76954090+ToastedFood@users.noreply.github.com>
2023-10-01 16:09:53 -0400

    created damaging spike

--- godot/Player/PlayerBox.gd
+++ godot/Player/PlayerBox.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+var Health = 2
+var box_sprite = [preload("res://assets/box.png"), preload("res://assets/boxdamaged1.png"), preload("res://assets/boxdamaged2.png")]
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -18,6 +19,10 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_left"):
 		$RigidBody2D.apply_central_impulse(Vector2(-100, 0))
 	"""
+func damage():
+	if(Health > 0):
+		Health -= 1
+		$RigidBody2D/Sprite.set_texture(box_sprite[2-Health])
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
